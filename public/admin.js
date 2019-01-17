88ee9a24790a1473ac4fbfea43451e70b52d70da
Barış Soner Uşaklı <barisusakli@gmail.com>
2019-01-17 12:04:29 -0500

    fix client side

--- public/admin.js
+++ public/admin.js
@@ -1,31 +1,37 @@
-/* globals app, $, socket, document */
+/* globals app, $, socket, define */
 
 'use strict';
 
-$(document).ready(function () {
-	$('#add-hook').on('click', function () {
-		app.parseAndTranslate('admin/plugins/webhooks', 'hooks', { hooks: [{ name: 'action:**.**', endpoint: 'https://yourendpoint.com' }] }, function (html) {
-			$('#hooks-parent').append(html);
-		});
-	});
+define('admin/plugins/webhooks', function () {
+	var WebHooks = {};
 
-	$('#save').on('click', function () {
-		var data = [];
-		$('#hooks-parent tr').each(function () {
-			data.push({
-				name: $(this).find('.hook-name').val(),
-				endpoint: $(this).find('.hook-endpoint').val(),
+	WebHooks.init = function () {
+		$('#add-hook').on('click', function () {
+			app.parseAndTranslate('admin/plugins/webhooks', 'hooks', { hooks: [{ name: 'action:**.**', endpoint: 'https://yourendpoint.com' }] }, function (html) {
+				$('#hooks-parent').append(html);
 			});
 		});
-		socket.emit('admin.plugins.webhooks.save', data, function (err) {
-			if (err) {
-				return app.alertError(err);
-			}
-			app.alertSuccess('Hooks Saved!');
-		});
-	});
 
-	$('#hooks-parent').on('click', '.hook-remove', function () {
-		$(this).parent().parent().remove();
-	});
+		$('#save').on('click', function () {
+			var data = [];
+			$('#hooks-parent tr').each(function () {
+				data.push({
+					name: $(this).find('.hook-name').val(),
+					endpoint: $(this).find('.hook-endpoint').val(),
+				});
+			});
+			socket.emit('admin.plugins.webhooks.save', data, function (err) {
+				if (err) {
+					return app.alertError(err);
+				}
+				app.alertSuccess('Hooks Saved!');
+			});
+		});
+
+		$('#hooks-parent').on('click', '.hook-remove', function () {
+			$(this).parent().parent().remove();
+		});
+	};
+
+	return WebHooks;
 });
